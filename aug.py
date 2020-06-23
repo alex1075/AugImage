31a042970ed89ace75d3655e63b4a9f8e4cafb51
Alex Hunt <alexander.hunt@ed.ac.uk>
2020-06-23 15:44:02 +0100

    yup

--- aug.py
+++ aug.py
@@ -1,8 +1,7 @@
 
 import imgaug as ia
 ia.seed(1)
-# imgaug uses matplotlib backend for displaying images
-%matplotlib inline
+
 from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 from imgaug import augmenters as iaa 
 # imageio library will be used for image input/output
@@ -20,6 +19,7 @@ import shutil
 images = []
 for index, file in enumerate(glob.glob('images/*.jpg')):
     images.append(imageio.imread(file))
+
     
 # how many images we have
 print('We have {} images'.format(len(images)))
@@ -28,10 +28,253 @@ print('We have {} images'.format(len(images)))
 for index, file in enumerate(glob.glob('images/*.jpg')):
     print('Image {} have size of {}'.format(file[7:], images[index].shape))
 
-# Have a loook at 2 smaller images
-ia.imshow(images[2])
-ia.imshow(images[7])
 
 # XML file names correspond to the image file names
 for index, file in enumerate(glob.glob('images/*.xml')):
     print(file[7:])
+
+# Function that will extract column data for our CSV file
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+# apply xml_to_csv() function to convert all XML files in images/ folder into labels.csv
+labels_df = xml_to_csv('images/')
+labels_df.to_csv(('labels.csv'), index=None)
+print('Successfully converted xml to csv.')
+
+# in previuos cell we also put all annotation in labels_df
+# let's see what's inside
+# each bounding box has a separate row
+# 2 pictures have two red pandas in it
+labels_df
+
+# start by groupping each image bounding boxes coordinates
+# separate dataframes are formed based on the filename
+grouped = labels_df.groupby('filename')
+# we can pull each group with get_group() using the filename
+group_df = group_df.reset_index()
+group_df = group_df.drop(['index'], axis=1)
+group_df
+
+# get bounding boxes coordinates from grouped data frame and write into array        
+bb_array = group_df.drop(['filename', 'width', 'height', 'class'], axis=1).values
+# display the array we've got
+bb_array
+
+
+
+# This setup of augmentation parameters will pick two of four given augmenters and apply them in random order
+aug = iaa.SomeOf(2, [    
+    iaa.Affine(rotate=(-90, 90)),
+    iaa.Affine(rotate=(-180, 0)),
+    iaa.Fliplr(1),
+    iaa.GaussianBlur(sigma=(1.0, 3.0)),
+    iaa.AdditiveGaussianNoise(scale=(0.03*255, 0.05*255))
+])
+
+def image_aug(df, images_path, aug_images_path, image_prefix, augmentor):
+    # create data frame which we're going to populate with augmented image info
+    aug_bbs_xy = pd.DataFrame(columns=
+                              ['filename','width','height','class', 'xmin', 'ymin', 'xmax', 'ymax']
+                             )
+    grouped = df.groupby('filename')
+    
+    for filename in df['filename'].unique():
+    #   get separate data frame grouped by file name
+        group_df = grouped.get_group(filename)
+        group_df = group_df.reset_index()
+        group_df = group_df.drop(['index'], axis=1)   
+    #   read the image
+        image = imageio.imread(images_path+filename)
+    #   get bounding boxes coordinates and write into array        
+        bb_array = group_df.drop(['filename', 'width', 'height', 'class'], axis=1).values
+    #   pass the array of bounding boxes coordinates to the imgaug library
+        bbs = BoundingBoxesOnImage.from_xyxy_array(bb_array, shape=image.shape)
+    #   apply augmentation on image and on the bounding boxes
+        image_aug, bbs_aug = augmentor(image=image, bounding_boxes=bbs)
+    #   disregard bounding boxes which have fallen out of image pane    
+        bbs_aug = bbs_aug.remove_out_of_image()
+    #   clip bounding boxes which are partially outside of image pane
+        bbs_aug = bbs_aug.clip_out_of_image()
+        
+    #   don't perform any actions with the image if there are no bounding boxes left in it    
+        if re.findall('Image...', str(bbs_aug)) == ['Image([]']:
+            pass
+        
+    #   otherwise continue
+        else:
+        #   write augmented image to a file
+            imageio.imwrite(aug_images_path+image_prefix+filename, image_aug)  
+        #   create a data frame with augmented values of image width and height
+            info_df = group_df.drop(['xmin', 'ymin', 'xmax', 'ymax'], axis=1)    
+            for index, _ in info_df.iterrows():
+                info_df.at[index, 'width'] = image_aug.shape[1]
+                info_df.at[index, 'height'] = image_aug.shape[0]
+        #   rename filenames by adding the predifined prefix
+            info_df['filename'] = info_df['filename'].apply(lambda x: image_prefix+x)
+        #   create a data frame with augmented bounding boxes coordinates using the function we created earlier
+            bbs_df = bbs_obj_to_df(bbs_aug)
+        #   concat all new augmented info into new data frame
+            aug_df = pd.concat([info_df, bbs_df], axis=1)
+        #   append rows to aug_bbs_xy data frame
+            aug_bbs_xy = pd.concat([aug_bbs_xy, aug_df])            
+    
+    # return dataframe with updated images and bounding boxes annotations 
+    aug_bbs_xy = aug_bbs_xy.reset_index()
+    aug_bbs_xy = aug_bbs_xy.drop(['index'], axis=1)
+    return aug_bbs_xy
+
+
+
+# function to convert BoundingBoxesOnImage object into DataFrame
+def bbs_obj_to_df(bbs_object):
+#     convert BoundingBoxesOnImage object into array
+    bbs_array = bbs_object.to_xyxy_array()
+#     convert array into a DataFrame ['xmin', 'ymin', 'xmax', 'ymax'] columns
+    df_bbs = pd.DataFrame(bbs_array, columns=['xmin', 'ymin', 'xmax', 'ymax'])
+    return df_bbs
+
+
+
+def resize_imgaug(df, images_path, aug_images_path, image_prefix):
+    # create data frame which we're going to populate with augmented image info
+    aug_bbs_xy = pd.DataFrame(columns=
+                              ['filename','width','height','class', 'xmin', 'ymin', 'xmax', 'ymax']
+                             )
+    grouped = df.groupby('filename')    
+    
+    for filename in df['filename'].unique():
+    #   Get separate data frame grouped by file name
+        group_df = grouped.get_group(filename)
+        group_df = group_df.reset_index()
+        group_df = group_df.drop(['index'], axis=1)
+        
+    #   The only difference between if and elif statements below is the use of height_resize and width_resize augmentors
+    #   deffined previously.
+
+    #   If image height is greater than or equal to image width 
+    #   AND greater than 600px perform resizing augmentation shrinking image height to 600px.
+        if group_df['height'].unique()[0] >= group_df['width'].unique()[0] and group_df['height'].unique()[0] > 600:
+        #   read the image
+            image = imageio.imread(images_path+filename)
+        #   get bounding boxes coordinates and write into array        
+            bb_array = group_df.drop(['filename', 'width', 'height', 'class'], axis=1).values
+        #   pass the array of bounding boxes coordinates to the imgaug library
+            bbs = BoundingBoxesOnImage.from_xyxy_array(bb_array, shape=image.shape)
+        #   apply augmentation on image and on the bounding boxes
+            image_aug, bbs_aug = height_resize(image=image, bounding_boxes=bbs)
+        #   write augmented image to a file
+            imageio.imwrite(aug_images_path+image_prefix+filename, image_aug)  
+        #   create a data frame with augmented values of image width and height
+            info_df = group_df.drop(['xmin', 'ymin', 'xmax', 'ymax'], axis=1)        
+            for index, _ in info_df.iterrows():
+                info_df.at[index, 'width'] = image_aug.shape[1]
+                info_df.at[index, 'height'] = image_aug.shape[0]
+        #   rename filenames by adding the predifined prefix
+            info_df['filename'] = info_df['filename'].apply(lambda x: image_prefix+x)
+        #   create a data frame with augmented bounding boxes coordinates using the function we created earlier
+            bbs_df = bbs_obj_to_df(bbs_aug)
+        #   concat all new augmented info into new data frame
+            aug_df = pd.concat([info_df, bbs_df], axis=1)
+        #   append rows to aug_bbs_xy data frame
+            aug_bbs_xy = pd.concat([aug_bbs_xy, aug_df])
+            
+    #   if image width is greater than image height 
+    #   AND greater than 600px perform resizing augmentation shrinking image width to 600px
+        elif group_df['width'].unique()[0] > group_df['height'].unique()[0] and group_df['width'].unique()[0] > 600:
+        #   read the image
+            image = imageio.imread(images_path+filename)
+        #   get bounding boxes coordinates and write into array        
+            bb_array = group_df.drop(['filename', 'width', 'height', 'class'], axis=1).values
+        #   pass the array of bounding boxes coordinates to the imgaug library
+            bbs = BoundingBoxesOnImage.from_xyxy_array(bb_array, shape=image.shape)
+        #   apply augmentation on image and on the bounding boxes
+            image_aug, bbs_aug = width_resize(image=image, bounding_boxes=bbs)
+        #   write augmented image to a file
+            imageio.imwrite(aug_images_path+image_prefix+filename, image_aug)  
+        #   create a data frame with augmented values of image width and height
+            info_df = group_df.drop(['xmin', 'ymin', 'xmax', 'ymax'], axis=1)        
+            for index, _ in info_df.iterrows():
+                info_df.at[index, 'width'] = image_aug.shape[1]
+                info_df.at[index, 'height'] = image_aug.shape[0]
+        #   rename filenames by adding the predifined prefix
+            info_df['filename'] = info_df['filename'].apply(lambda x: image_prefix+x)
+        #   create a data frame with augmented bounding boxes coordinates using the function we created earlier
+            bbs_df = bbs_obj_to_df(bbs_aug)
+        #   concat all new augmented info into new data frame
+            aug_df = pd.concat([info_df, bbs_df], axis=1)
+        #   append rows to aug_bbs_xy data frame
+            aug_bbs_xy = pd.concat([aug_bbs_xy, aug_df])
+
+    #     append image info without any changes if it's height and width are both less than 600px 
+        else:
+            aug_bbs_xy = pd.concat([aug_bbs_xy, group_df])
+    # return dataframe with updated images and bounding boxes annotations 
+    aug_bbs_xy = aug_bbs_xy.reset_index()
+    aug_bbs_xy = aug_bbs_xy.drop(['index'], axis=1)
+    return aug_bbs_xy
+
+
+
+# apply resizing augmentation to our images and write the updated images and bounding boxes annotations to the DataFrame 
+# we will not apply prefix to our files and will overwrite images in the same directory
+resized_images_df = resize_imgaug(labels_df, 'images/', 'images/', '')
+
+grouped_resized = resized_images_df.groupby('filename')
+grouped_augmented = augmented_images_df.groupby('filename')
+
+for filename in resized_images_df['filename'].unique():    
+    
+    group_r_df = grouped_resized.get_group(filename)
+    group_r_df = group_r_df.reset_index()
+    group_r_df = group_r_df.drop(['index'], axis=1)
+    bb_r_array = group_r_df.drop(['filename', 'width', 'height', 'class'], axis=1).values
+    resized_img = imageio.imread('images/'+filename)
+    bbs_r = BoundingBoxesOnImage.from_xyxy_array(bb_r_array, shape=resized_img.shape)
+    
+    group_a_df = grouped_augmented.get_group('aug1_'+filename)
+    group_a_df = group_a_df.reset_index()
+    group_a_df = group_a_df.drop(['index'], axis=1)
+    bb_a_array = group_a_df.drop(['filename', 'width', 'height', 'class'], axis=1).values
+    augmented_img = imageio.imread('aug_images/'+'aug1_'+filename)
+    bbs_a = BoundingBoxesOnImage.from_xyxy_array(bb_a_array, shape=augmented_img.shape)
+    
+    ia.imshow(np.hstack([
+            bbs_r.draw_on_image(resized_img, size=2),
+            bbs_a.draw_on_image(augmented_img, size=2)
+            ]))
+
+
+
+# Concat resized_images_df and augmented_images_df together and save in a new all_labels.csv file
+all_labels_df = pd.concat([resized_images_df, augmented_images_df])
+all_labels_df.to_csv('all_labels.csv', index=False)
+
+# Lastly we can copy all our augmented images in the same folder as original resized images
+for file in os.listdir('aug_images'):
+    shutil.copy('aug_images/'+file, 'images/'+file)
+
+# Apply augmentation to our images and save files into 'aug_images/' folder with 'aug1_' prefix.
+# Write the updated images and bounding boxes annotations to the augmented_images_df dataframe.
+augmented_images_df = image_aug(resized_images_df, 'images/', 'aug_images/', 'aug1_', aug)
+
+for thing in images:
+    image_aug(thing)
+
---
+++ images/ZeFlip.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def update_image(self):
+    if self.kwargs["how"] == "vertical":
+        img = np.flipud(self.img)
+    elif self.kwargs["how"] == "horizontal":
+        img = np.fliplr(self.img)
+    return img
+
+if self.kwargs["how"] == "vertical":
+    for box in self.annotations["boxes"]:
+        name = box['label']
+        # unchanged
+        width = box["width"]
+        # unchanged
+        height = box["height"]
+        # check if annotation is on bottom of image
+        if box["y"] < img_height/2:
+            # new Y position is addition of center and diff of
+            # (center and old y)
+            nY = img_height/2 + (img_height/2 - box["y"])
+        # else annotation is on right side of image
+        elif box["y"] >= img_height/2:
+            # new Y position is center  diff between old x and center
+            nY = img_height/2 - (box["y"] - img_height/2)
+        # unchanged
+        nX = box["x"]
+        # create new boxes for each label
+        new_box.append({
+        "label": name,
+        "x": nX,
+        "y": nY,
+        "width": width,
+        "height": height
+})
